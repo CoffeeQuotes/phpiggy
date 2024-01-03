d9a222397d3604f4b0a27ba8c24b135460e64e2c
Shishir Kumar <sky8052785942ocean@gmail.com>
2024-01-04 01:45:26 +0530

    refactored routes

--- src/App/functions.php
+++ src/App/functions.php
@@ -1,10 +1,16 @@
-<?php 
+<?php
+
 declare(strict_types=1);
 
-function dd(mixed $val) 
+function dd(mixed $val)
 {
     echo '<pre>';
     var_dump($val);
     echo '</pre>';
     die();
+}
+
+function e(mixed $value): string
+{
+    return htmlspecialchars((string) $value);
 }
