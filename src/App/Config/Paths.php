b10e3667fad9c731876b3ed724cc52530b851863
Shishir Kumar <sky8052785942ocean@gmail.com>
2024-01-04 01:10:29 +0530

    before adding about

---
+++ src/App/Config/Paths.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Config;
+
+class Paths
+{
+    public const VIEW = __DIR__ . "/../views";
+
+    /* public const ROOT_DIR = __DIR__ . '/../..';
+    public const APP_DIR = self::ROOT_DIR . '/app';
+    public const CONFIG_DIR = self::ROOT_DIR . '/config';
+    public const CONTROLLERS_DIR = self::APP_DIR . '/Controllers';
+    public const MODELS_DIR = self::APP_DIR . '/Models';
+    public const VIEWS_DIR = self::APP_DIR . '/Views';
+    public const PUBLIC_DIR = self::ROOT_DIR . '/public';
+    public const CACHE_DIR = self::ROOT_DIR . '/cache';
+    public const LOGS_DIR = self::ROOT_DIR . '/logs';
+    public const RESOURCES_DIR = self::ROOT_DIR . '/resources';
+    public const VENDOR_DIR = self::ROOT_DIR . '/vendor';
+    public const LIBRARY_DIR = self::VENDOR_DIR . '/library';
+    public const LIBRARY_VIEWS_DIR = self::LIBRARY_DIR . '/Views'; */
+}
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -1,21 +1,53 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
 namespace Framework;
 
 
-class Router 
+class Router
 {
     private array $routes = [];
 
-    public function add(string $method, string $path): void 
+    public function add(string $method, string $path, array $controller): void
     {
         $this->routes[] = [
-            'path' => $path,
+            'path' => $this->normalizePath($path),
             'method' => strtoupper($method),
+            'controller' => $controller
         ];
-    } 
+    }
 
+    private function normalizePath(string $path): string
+    {
+        /* trim will remove forward slahses (& whitespaces) from beginning and end of $path */
+        $path =  trim($path, '/');
+        $path = "/{$path}/";
+        /* Preg_replace will replace all forward slashes with one forward slash  */
+        $path = preg_replace('#[/]{2,}#', '/', $path);
+        return $path;
+    }
 
+    public function dispatch(string $path, string $method): void
+    {
+        $method = strtoupper($method);
+        $path = $this->normalizePath($path);
+
+        foreach ($this->routes as $route) {
+            /**
+             * We are using preg_match to check if $path matches $route['path'] instead of using comparsion operator because of we want to support route parameters.  
+             * */
+            if (!preg_match("#^{$route['path']}$#", $path) || $route['method'] !== $method) {
+                continue;
+            }
+            $controller = $route['controller'];
+            [$class, $function] = $controller;
+            $controllerInstance = new $class();
+            $controllerInstance->{$function}();
+            return;
+        }
+
+        // http_response_code(404);
+        // echo "Page not found";
+    }
 }
