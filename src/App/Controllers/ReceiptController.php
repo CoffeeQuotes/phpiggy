7d06a5bcd4b139a295704d6573ea73c9f402b703
Shishir Kumar <sky8052785942ocean@gmail.com>
2024-01-19 01:39:26 +0530

    receipt section completed

--- src/App/Controllers/ReceiptController.php
+++ src/App/Controllers/ReceiptController.php
@@ -40,4 +40,40 @@ class ReceiptController
         $this->receiptService->upload($receiptFile, $transaction['id']);
         redirectTo("/");
     }
+
+    public function download(array $params)
+    {
+        $transaction = $this->transactionService->getUserTransaction($params['transaction']);
+        if (empty($transaction)) {
+            redirectTo('/');
+        }
+        $receipt = $this->receiptService->getReceipt($params['receipt']);
+        if (empty($receipt)) {
+            redirectTo('/');
+        }
+
+        if ($receipt['transaction_id'] !== $transaction['id']) {
+            redirectTo('/');
+        }
+        $this->receiptService->read($receipt);
+    }
+
+    public function delete(array $params)
+    {
+        $transaction = $this->transactionService->getUserTransaction($params['transaction']);
+        if (empty($transaction)) {
+            redirectTo('/');
+        }
+        $receipt = $this->receiptService->getReceipt($params['receipt']);
+        if (empty($receipt)) {
+            redirectTo('/');
+        }
+
+        if ($receipt['transaction_id'] !== $transaction['id']) {
+            redirectTo('/');
+        }
+        $this->receiptService->delete($receipt);
+
+        redirectTo('/');
+    }
 }
